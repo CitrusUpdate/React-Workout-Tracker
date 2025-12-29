f4a3942c7998c2f3251f405170b7f841e32cba09
Szymon Napierała <szymeknapierala08@gmail.com>
2025-12-29 23:17:24 +0100

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
-import mongoose from "mongoose";
 import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
 
 dotenv.config();
 const __dirname = path.resolve();
@@ -15,12 +17,9 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-    res.send("Gym Tracker API is running");
-});
 
 if(NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -32,4 +31,5 @@ if(NODE_ENV === "production") {
 
 app.listen(PORT, () => {
     console.log("Server running on port ", PORT);
+    connectDB();
 });
