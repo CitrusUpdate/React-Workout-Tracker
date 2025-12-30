e37d11cc4e16ac04071ad1f633baae6f5474c8ae
Szymon Napierała <szymeknapierala08@gmail.com>
2025-12-30 19:14:50 +0100

    Arcjet protection and protect route added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,4 +5,6 @@ export const ENV = {
     MONGO_URL: process.env.MONGO_URL,
     NODE_ENV: process.env.NODE_ENV,
     JWT_SECRET: process.env.JWT_SECRET,
+    ARCJET_KEY: process.env.ARCJET_KEY,
+    ARCJET_ENV: process.env.ARCJET_ENV,
 };
