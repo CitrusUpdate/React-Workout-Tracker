39c94dca65a795ce9e48156f65f2d5dac8d48e01
Szymon Napierała <szymeknapierala08@gmail.com>
2026-03-20 15:48:43 +0100

    analytics services added

--- backend/src/job/analytics.job.js
+++ backend/src/job/analytics.job.js
@@ -1,13 +1,14 @@
 import cron from "node-cron";
+import { runProgressAnalytics, runHighRepsAnalysis, runRirAnalysis } from "../services/analytics.service";
 
 export const startAnalyticsJob = () => {
     cron.schedule("0 2 * * *", async () => {
         console.log("Running analytics job...");
 
         try {
-            /*
-                Algorithms for analytics here
-            */
+            await runProgressAnalytics();
+            await runHighRepsAnalysis();
+            await runRirAnalysis();
         } catch(error) {
             console.error("Analytics job error", error);
         }
