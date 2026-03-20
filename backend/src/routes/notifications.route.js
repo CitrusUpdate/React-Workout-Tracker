a3c36b50d8f347ec73bcbe80aed9c439f274a504
Szymon Napierała <szymeknapierala08@gmail.com>
2026-03-20 14:17:01 +0100

    Notifications system added

---
+++ backend/src/routes/notifications.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { getNotifications, markAsRead, clearNotifications } from "../controllers/notifications.controller.js";
+import { protectRoute } from "../middleware/auth.middleware";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.patch("/:id/read", protectRoute, markAsRead);
+router.delete("/clear", protectRoute, clearNotifications);
+
+export default router;
